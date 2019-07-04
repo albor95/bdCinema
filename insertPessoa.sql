97bffc032ee1296a27907084dd25de8d56d3a0e1
albor95 <47107868+albor95@users.noreply.github.com>
2019-07-03 22:10:04 -0300

    Create insertPessoa.sql
    close #10

---
+++ insertPessoa.sql
@@ -0,0 +1,3 @@
+insert ignore into pessoa(nome,tipo,cpf,dataNsc,cep,rua,numero,bairro) values(
+'nome1','cliente','111.111.111-11','1111-11-11','11111-11','Rua1','1111','bairro1'
+),('nome2','admin','222.222.222-22','2222-22-22','22222-22','Rua2','2222','bairro2');
