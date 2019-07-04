64352c393334d5a57d833aa47167598e3c8a0db4
albor95 <47107868+albor95@users.noreply.github.com>
2019-07-03 21:01:25 -0300

    Update Sessao.sql

--- Sessao.sql
+++ Sessao.sql
@@ -2,6 +2,7 @@ create table sessao(
 filmeFk int,
 salaFk int,
 SesDatHor datetime,
+idioma varchar(20),
 constraint PK_Sessao Primary Key (filmeFk, salaFk, SesDatHor),
 Foreign Key (filmeFk) References filme (idFilme),
 Foreign Key (salaFk) References sala (idSala)
